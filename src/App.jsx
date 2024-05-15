48f8375b2f7162ac200f7d1e25dadb1a8f893aa1
Ivylegend <victoryicha2@gmail.com>
2024-05-15 11:11:46 +0100

    collaboration, pricing, roadmap, services, tagline components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Services from "./components/Services";
+import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -10,6 +15,11 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
